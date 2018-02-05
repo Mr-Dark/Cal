609122f68c3772f653d0ae04f928648564009a3d
bara <bara@172.16.232.196>
2018-02-05 13:58:18 +0200

    suck it jin

--- src/Modle.java
+++ src/Modle.java
@@ -1,4 +1,28 @@
-
+import java.lang.Math;
 public class Modle {
+	
+public double add(double x,double y) {
+	
+	return x+y;
+}
+public double sub(double x,double y) {
+	
+	return x-y;
+}
+public double mul(double x,double y) {
+	
+	return x*y;
+}
+public double div(double x,double y) {
+	
+	return x/y;
+}
+public double mod(double x,double y) {
+	
+	return x%y;
+}
+public double sq(double x) {
+	return Math.sqrt(x);
+}
 
 }
