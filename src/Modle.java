b129000fcf72620ae79f044c8e40b43df874c865
Jamal-Alkelani <gamalalkelani@gmail.com>
2018-02-12 16:51:45 +0200

    try #1

--- src/Modle.java
+++ src/Modle.java
@@ -1,8 +1,9 @@
 import java.lang.Math;
+
+import javax.swing.JOptionPane;
 public class Modle {
 	
 public double add(double x,double y) {
-	
 	return x+y;
 }
 public double sub(double x,double y) {
@@ -14,7 +15,10 @@ public double mul(double x,double y) {
 	return x*y;
 }
 public double div(double x,double y) {
-	
+	if(y==0)
+	{
+		return 0;
+	}
 	return x/y;
 }
 public double mod(double x,double y) {
@@ -22,6 +26,10 @@ public double mod(double x,double y) {
 	return x%y;
 }
 public double sq(double x) {
+	if(x<0) {
+	 x=x*-1;
+	}
+		
 	return Math.sqrt(x);
 }
 
